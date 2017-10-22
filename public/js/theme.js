780070d8b163a378da1c5e0877c7f14551ee6dd6
Holger Woltersdorf <hw@hollo.me>
2017-10-23 00:21:27 +0200

    Adds inline doc toggle

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,18 +1,19 @@
-(function ()
-{
-	var sidebar       = document.getElementById( 'sidebar' );
-	var sidebarToggle = document.getElementById( 'sidebar-toggle' );
-	sidebarToggle.addEventListener( 'click', function ( e )
-	{
-		if ( sidebarToggle.getAttribute( 'class' ) === 'open' )
-		{
-			sidebar.removeAttribute( 'class' );
-			sidebarToggle.removeAttribute( 'class' );
-		}
-		else
-		{
-			sidebar.setAttribute( 'class', 'open' );
-			sidebarToggle.setAttribute( 'class', 'open' );
-		}
-	} );
+(function () {
+    var sidebar = document.getElementById('sidebar');
+    var sidebarToggle = document.getElementById('sidebar-toggle');
+    sidebarToggle.addEventListener('click', function (e) {
+        if (sidebarToggle.getAttribute('class') === 'open') {
+            sidebar.removeAttribute('class');
+            sidebarToggle.removeAttribute('class');
+        }
+        else {
+            sidebar.setAttribute('class', 'open');
+            sidebarToggle.setAttribute('class', 'open');
+        }
+    });
+    $('a[data-toggle]').click(function (e) {
+        e.preventDefault();
+        var target = $($(this).data('toggle'));
+        target.toggle();
+    });
 })();
